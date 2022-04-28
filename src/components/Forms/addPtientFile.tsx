5fe06bc02f6c8d3c81bbcf025b2cb696c263b739
Redouane Bouabana <rredouane342@gmail.com>
2022-04-28 14:40:20 +0000

    add agent login and patient | add patient form

---
+++ src/components/Forms/addPtientFile.tsx
@@ -0,0 +1,169 @@
+import {Field, FieldArray, Form, Formik} from 'formik';
+import React from 'react';
+
+import {useNavigate} from 'react-router-dom';
+
+const AddPatientFile = (setIsOpen: any) => {
+    let navigate = useNavigate();
+
+    return (
+        <Formik
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                password: "",
+                phone: '',
+                address: '',
+                city: '',
+                medicine: '',
+                email: '',
+                file: '',
+                friends: []
+            }}
+            onSubmit={(values: any) => {
+                // let data = new FormData();
+                // data.append('firstName', values.firstName);
+                // data.append('lastName', values.lastName);
+                // data.append('password', values.password);
+                // data.append('medicine', values.medicine);
+                // data.append('email', values.email);
+                // data.append('file', values.file);
+                // @ts-ignore
+                console.log(values)
+
+            }}
+        >
+            {({errors, touched, setFieldValue , values}: any) => (
+                <Form>
+                    {/* {error && (
+                        <div className="py-2 px-3 bg-red-500 w-full text-white rounded-md flex justify-between">
+                            {error} <button onClick={() => setError('')}>X</button>
+                        </div>
+                    )} */}
+
+                    <div className="mt-4">
+                        <label htmlFor="firstName"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            FirstName
+                        </label>
+                        <Field
+                            type="text"
+                            id="firstName"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            name="firstName"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label htmlFor="lastName"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            LastName
+                        </label>
+                        <Field
+                            type="text"
+                            id="lastName"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            name="lastName"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label htmlFor="email"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            Email
+                        </label>
+                        <Field
+                            type="text"
+                            id="email"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            name="email"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label htmlFor="email"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            Phone
+                        </label>
+                        <Field
+                            type="text"
+                            id="phone"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            name="phone"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label htmlFor="email"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            medicine
+                        </label>
+                        <Field
+                            type="text"
+                            id="medicine"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            name="medicine"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label htmlFor="email"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            city
+                        </label>
+                        <Field
+                            type="text"
+                            id="city"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            name="city"
+                        />
+                    </div>
+
+                    <div className="mt-4">
+                        <label htmlFor="file"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                            Upload patient file
+                        </label>
+                        <input
+                            className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none focus:border-transparent dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                            id="file"
+                            name="file"
+                            type="file"
+                            onChange={(e) => {
+                                e.target.files && setFieldValue('file', e.target.files[0]);
+                            }}
+                        />
+                    </div>
+                    <FieldArray
+                        name="friends"
+                        render={arrayHelpers => (
+                            <div>
+                                {values.friends.map((friend, index) => (
+                                    <div key={index}>
+                                        {/** both these conventions do the same */}
+                                        <Field name={`friends[${index}].name`} />
+                                        <Field name={`friends.${index}.age`} />
+
+                                        <button type="button" onClick={() => arrayHelpers.remove(index)}>
+                                            -
+                                        </button>
+                                    </div>
+                                ))}
+                                <button
+                                    type="button"
+                                    onClick={() => arrayHelpers.push({ name: '', age: '' })}
+                                >
+                                    +
+                                </button>
+                            </div>
+                        )}
+                    />
+                    <div className="mt-8 flex justify-between">
+                        <button
+                            type="submit"
+                            className="w-[12em] text-green-900 bg-white border border-green-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-500 dark:text-white dark:border-green-600 dark:hover:bg-green-700 dark:hover:border-gray-700 dark:focus:ring-green-800"
+                        >
+                            add Patient File
+                        </button>
+                    </div>
+                </Form>
+            )}
+        </Formik>
+    );
+};
+export default AddPatientFile;
---
+++ src/components/Tabels/PatientTable.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useState } from 'react';
+
+
+const PatientTable = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const data : any = []
+    const [PatientId, setPatientId] = useState('');
+    return (
+        <div className="inline-block py-2 min-w-full ">
+            <div className="overflow-hidden shadow-md sm:rounded-lg">
+                <table className="min-w-full">
+                    <thead className="bg-gray-50 dark:bg-gray-700">
+                    <tr>
+                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                            Name
+                        </th>
+                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                            email
+                        </th>
+                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                            phone
+                        </th>
+                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                            city
+                        </th>
+                        <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                            file
+                        </th>
+                        <th scope="col" className="relative py-3 px-6">
+                            <span className="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data?.map(
+                        (driver : any) =>
+                            !driver.verified && (
+                                <tr key={driver.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{driver.firstName} {driver.lastName} </td>
+                                    <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">{driver.email}</td>
+                                    <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">{driver.license}</td>
+                                    <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">{driver.createdAt}</td>
+                                    <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">{driver.file}</td>
+                                    <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                        <button
+                                            type="button"
+                                            onClick={() => {
+                                                setIsOpen(!isOpen);
+                                                setPatientId(driver._id);
+                                            }}
+                                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                        >
+                                            recruit
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                    )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default PatientTable;
